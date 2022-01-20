4b24b1aa10988369df5a147925ef1c2f5ebe87bd
Oleg Matviichuk <ma7hway@gmail.com>
2022-01-20 18:47:13 +0200

    prettier fix

--- client/components/home.js
+++ client/components/home.js
@@ -13,10 +13,10 @@ const Home = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <Switch>
-              <Route exact path="/dashboard" component={() => <Dashboard />} />
-              <Route exact path="/dashboard/main" component={() => <Main />} />
-              <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
-            </Switch>
+            <Route exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/dashboard/main" component={() => <Main />} />
+            <Route exact path="/dashboard/profile/:username" component={() => <Profile />} />
+          </Switch>
         </div>
       </div>
     </div>
