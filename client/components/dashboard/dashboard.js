31f0efe579862e0f6819a62385bb971266ee9ec3
Oleg Matviichuk <ma7hway@gmail.com>
2022-01-20 19:12:04 +0200

    trial title test fix

--- client/components/dashboard/dashboard.js
+++ client/components/dashboard/dashboard.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import Head from '../head'
-
 const Dashboard = () => {
   return (
-    <div id="title">Dashboard
-      <Head title="Hello" />
+    <div>
       <div className="flex items-center justify-center">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+        <div id="title">Dashboard</div>
           <div>
             <Link to="/dashboard/profile/04a5b97e-6681-4a48-a3bb-229bb9e405c2">Go To Profile</Link>
           </div>
